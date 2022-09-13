2c50bfc59754819746ec1f203a8bb87f3e3a37af
Pedro Fonseca <pedrinhopedro01@gmail.com>
2022-09-13 09:39:07 -0300

    refactor: refatorado o arquivo server.js

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,12 +1,16 @@
+import 'reflect-metadata';
 import express from 'express';
 import cors from 'cors';
 import routes from '../routes';
+import httpErrorMiddleware from '../../middleware/http.error.middleware';
+import '@shared/typeorm';
 
 const app = express();
 const port = 3000;
 
 app.use(cors());
 app.use(routes);
+app.use(httpErrorMiddleware);
 
 app.get('/', (req, res) => res.send('Hello World!'));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
